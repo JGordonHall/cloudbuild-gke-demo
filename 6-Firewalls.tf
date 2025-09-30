978e3221eb2fc83a3b9c2396f25de108c53e6b72
JGordonHall <jarred.p.hall@gmail.com>
2025-09-30 14:50:49 -0700

    Fix: restrict SSH access and disable GKE client certs

--- 6-Firewalls.tf
+++ 6-Firewalls.tf
@@ -2,11 +2,10 @@
 resource "google_compute_firewall" "allow-ssh" {
   name    = "allow-ssh"
   network = google_compute_network.main.name
-
   allow {
     protocol = "tcp"
-    ports    = ["80", "22"]
+    ports    = ["22"]
   }
-
-  source_ranges = ["0.0.0.0/0"]
+  # restrict to your trusted IP or corporate range
+  source_ranges = ["203.0.113.15/32"]
 }
--- 7-Kubernetes.tf
+++ 7-Kubernetes.tf
@@ -1,21 +1,52 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster
+// 7-Kubernetes.tf
 resource "google_container_cluster" "primary" {
-  name                     = "primary"
-  location                 = "us-central1"
+  name     = "primary"
+  location = "us-central1"
+
+  # Networking
+  network    = google_compute_network.main.self_link
+  subnetwork = google_compute_subnetwork.private.self_link
+
+  # Regional cluster for HA
   remove_default_node_pool = true
   initial_node_count       = 1
-  network                  = google_compute_network.main.self_link
-  subnetwork               = google_compute_subnetwork.private.self_link
-  logging_service          = "logging.googleapis.com/kubernetes"
-  monitoring_service       = "monitoring.googleapis.com/kubernetes"
-  networking_mode          = "VPC_NATIVE"
-  deletion_protection = false
 
-  # Optional, if you want multi-zonal cluster
-  node_locations = [
-    "us-central1-c"
-  ]
+  # Security: disable legacy ABAC and client certificates
+  enable_legacy_abac = false
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
 
+  # Private cluster config
+  private_cluster_config {
+    enable_private_nodes   = true
+    master_ipv4_cidr_block = "172.16.0.0/28"
+  }
+
+  # Release channel
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  # Workload Identity for secure service account mapping
+  workload_identity_config {
+    workload_pool = "cb-pipeline-demo.svc.id.goog"
+  }
+
+  # Logging & Monitoring
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+
+  # IP allocation
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "k8s-pod-range"
+    services_secondary_range_name = "k8s-service-range"
+  }
+
+  # Addons
   addons_config {
     http_load_balancing {
       disabled = true
@@ -25,24 +56,13 @@ resource "google_container_cluster" "primary" {
     }
   }
 
-  release_channel {
-    channel = "REGULAR"
+  # Labels for tracking
+  resource_labels = {
+    environment = "staging"
+    owner       = "devops"
   }
+}
 
-  workload_identity_config {
-    workload_pool = "cb-pipeline-demo.svc.id.goog"
-  }
-
-  ip_allocation_policy {
-    cluster_secondary_range_name  = "k8s-pod-range"
-    services_secondary_range_name = "k8s-service-range"
-  }
-
-  private_cluster_config {
-    enable_private_nodes    = true
-    enable_private_endpoint = false
-    master_ipv4_cidr_block  = "172.16.0.0/28"
-  }
 
   #   Jenkins use case
   #   master_authorized_networks_config {
@@ -51,4 +71,4 @@ resource "google_container_cluster" "primary" {
   #       display_name = "private-subnet-w-jenkins"
   #     }
   #   }
-}
+
